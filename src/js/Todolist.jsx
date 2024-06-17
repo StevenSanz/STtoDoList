86dc503400cf0355f1582d5a4011232160003a3d
StevenSanz <163450220+StevenSanz@users.noreply.github.com>
2024-06-17 12:55:16 +0000

    actualizadoSST

--- src/js/Todolist.jsx
+++ src/js/Todolist.jsx
@@ -13,15 +13,20 @@ export const Todolist = () => {
             setInputValue('');
         }
       };
+      const deleteTask = (index) => {
+        setTasklist(taskList.filter((_, i) => i !== index));
+      };
+    
+    
 
     return (
         <div className="container">
             <div className="mb-2">
                 <input type="text" value={inputValue} onChange={handleInputChange} onKeyDown={handleKeyDown} />
             </div>
-                <ul>
+                <ul className="d-flex flex-column" > 
                     {taskList.map((item, index) => (
-					<li className='fs-2 ms-3 p-2 bg-secondary text-white rounded border border-danger' key={index}>{item}</li>
+					<li className='fs-2 ms-3 p-2 bg-dark text-white rounded border border-danger d-flex justify-content-center' key={index}>{item}    <div className="d-flex ms-auto"><button type="button" onClick={() => deleteTask(index)} className="btn btn-dark">borrar</button></div></li>
 				    ))}
                 </ul>
         </div>
