37ae4dfac3dab56836393adceb4dda23d18c63a8
StevenSanz <163450220+StevenSanz@users.noreply.github.com>
2024-06-23 16:51:36 +0000

    STpulir

--- src/js/Todolist.jsx
+++ src/js/Todolist.jsx
@@ -5,7 +5,7 @@ import {faCircleXmark} from '@fortawesome/free-solid-svg-icons';
 export const Todolist = () => {
     const [taskList, setTasklist] = useState(['Make the bed','Eat','Walk the dog', 'Coding']);
     const [inputValue, setInputValue] = useState(''); 
-    const [showButton, setShowButton] = useState(false);
+    const [showButton, setShowButton] = useState(null);
 
 
     const inputChange = (e) => {
@@ -23,12 +23,12 @@ export const Todolist = () => {
         setTasklist(taskList.filter((_, i) => i !== index));
       };
 
-      const handleMouseEnter = () => {
-        setShowButton(true);
+      const handleMouseEnter = (index) => {
+        setShowButton(index);
       };
     
       const handleMouseLeave = () => {
-        setShowButton(false);
+        setShowButton(null);
       };
     return (
         <div className="container">
@@ -40,10 +40,10 @@ export const Todolist = () => {
             {taskList.length > 0 ? (
                 <ul className="d-flex flex-column bg-dark ps-0" > 
                     {taskList.map((item, index) => (
-					          <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
+					          <li onMouseEnter={() => handleMouseEnter(index)} onMouseLeave={handleMouseLeave}
                     className="fs-2 ms-3 p-2 text-white rounded border border-danger d-flex justify-content-center" key={index}>{item}    
-                    <div className="d-flex ms-auto"> {showButton && <button type="button" onClick={() => deleteTask(index)} className="btn btn-dark">
-                      <FontAwesomeIcon icon={faCircleXmark} /></button>}</div></li>
+                    <div className="d-flex ms-auto"> {showButton === index && (<button type="button" onClick={() => deleteTask(index)} className="btn btn-dark">
+                      <FontAwesomeIcon icon={faCircleXmark} /></button>)}</div></li>
                     ))}
                     <li className="fs-5 d-flex ms-3 me-auto bg-dark text-white">{taskList.length} items left</li>
                 </ul>
